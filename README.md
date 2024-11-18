b448853b3b4611fe127f0f049f80812ad72e4d2a
Prajwal Shetty <prajwalshetty3000@gmail.com>
2024-11-18 00:48:14 +0000

    Basic testing, up and running

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+### Set API Key:
+Set the environment variable `PS_OPENAIAPIKEY` to your API key.
+In windows you can use[:]()
+```cmd
+setx PS_OPENAIAPIKEY "your api key"
+```
---
+++ Source/GenerativeAISupport/Public/GenSecureKey.h
@@ -0,0 +1,44 @@
+// Copyright Prajwal Shetty 2024. All rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+
+#if PLATFORM_WINDOWS
+#include "Runtime/Core/Public/Windows/WindowsPlatformMisc.h"
+#endif
+
+#if PLATFORM_MAC
+#include "Runtime/Core/Public/Apple/ApplePlatformMisc.h"
+#endif
+
+#if PLATFORM_LINUX
+#include "Runtime/Core/Public/Linux/LinuxPlatformMisc.h"
+#endif
+
+#include "GenSecureKey.generated.h"
+
+UCLASS()
+class GENERATIVEAISUPPORT_API UGenSecureKey : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	// Set the API key for Generative AI
+	UFUNCTION(BlueprintCallable, Category = "Generative AI")
+	static void SetGenerativeAIApiKey(FString apiKey);
+
+	// Get the API key for Generative AI
+	static FString GetGenerativeAIApiKey();
+
+	// Set whether to use the API key from environment variables
+	UFUNCTION(BlueprintCallable, Category = "Generative AI")
+	static void SetUseApiKeyFromEnvironmentVars(bool bUseEnvVariable);
+
+	// Get whether to use the API key from environment variables
+	static bool GetUseApiKeyFromEnvironmentVars();
+
+	// Retrieve the environment variable based on key
+	static FString GetEnvironmentVariable(FString key);
+};
